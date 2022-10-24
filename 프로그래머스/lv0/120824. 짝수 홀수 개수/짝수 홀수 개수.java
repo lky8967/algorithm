8a23637fa13f35c038fa869485c0e020b74aa4eb
lky8967 <104722856+lky8967@users.noreply.github.com>
2022-10-24 23:29:30 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,16 +1,13 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[2];
-        int q = 0;
-        int w = 0;
+        int[] answer = {};
+        answer = new int[2];
         for(int i = 0 ; i < num_list.length ; i++){
             if(num_list[i] % 2 ==0){
-                q++;
+                answer[0]++;
             } else {
-                w++;
+                answer[1]++;
             }
-            answer[0] = q;
-            answer[1] = w;
         }
         return answer;
     }
