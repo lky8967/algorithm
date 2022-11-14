ee86686c168d316c5ebfe27d462a041623a59b00
lky8967 <104722856+lky8967@users.noreply.github.com>
2022-11-14 20:29:22 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 3.04 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        
+        for (int i = 0 ; i < n ; i++){
+            System.out.print(i);
+            if(i%2 == 1){
+                answer += '박';
+            } else {
+                answer += '수';
+            }
+        } 
+        
+        return answer;
+    }
+}
