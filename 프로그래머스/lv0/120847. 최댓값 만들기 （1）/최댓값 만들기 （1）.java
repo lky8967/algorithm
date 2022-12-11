5e74fd332df3902fb022f5a3d53a2c07297dd202
lky8967 <104722856+lky8967@users.noreply.github.com>
2022-12-11 17:59:38 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int max = 0;
+        int maxx = 0;
+        for(int i=0 ; i<numbers.length ; i++){
+            for(int j=0 ; j<numbers.length ; j++){
+			    if(i != j && numbers[i] * numbers[j] > max){
+                    max = numbers[i] * numbers[j];
+                }
+            }
+		}
+        answer = max;
+        return answer;
+    }
+}
