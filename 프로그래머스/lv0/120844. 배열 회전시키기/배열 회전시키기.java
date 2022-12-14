8eb96da7c4e4689e99a6d986f9c60b9e82f02a0d
lky8967 <104722856+lky8967@users.noreply.github.com>
2022-12-14 17:02:48 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.02 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] numbers, String direction) {
+        int[] answer = new int[numbers.length];
+        for(int i=0; i<numbers.length; i++){
+            if(direction.equals("right")){
+                if(i < numbers.length-1){
+                    answer[i+1] = numbers[i];
+                }else{
+                    answer[0] = numbers[i];
+                }
+            }else{
+                if(i == 0){
+                    answer[numbers.length-1] = numbers[0];
+                }else{
+                    answer[i-1] = numbers[i];
+                }
+            }
+        }
+        return answer;
+    }
+}
