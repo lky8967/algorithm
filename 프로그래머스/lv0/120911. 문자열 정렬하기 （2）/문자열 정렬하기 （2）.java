35cc96d21eebb0a7d836fdca272901f7939afcb7
lky8967 <104722856+lky8967@users.noreply.github.com>
2023-01-24 22:36:30 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 2.54 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        String[] arr = my_string.toLowerCase().split("");
+
+        Arrays.sort(arr);
+        
+        for(int i = 0 ; i < arr.length; i++){
+            answer = answer + arr[i];
+        }
+        return answer;
+    }
+}
