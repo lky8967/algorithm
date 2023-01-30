61561299ee675015426a6411ffcdc169f0e75ddc
lky8967 <104722856+lky8967@users.noreply.github.com>
2023-01-30 19:11:18 +0900

    [level 0] Title: k의 개수, Time: 2.11 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,19 +1,16 @@
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
-        for (int q = i; q <= j; q++) {
-            StringBuilder sb = new StringBuilder();
-            sb.append(q);
-
-            int defaultLength = sb.length();
-
-            for (int r = 0; r < defaultLength; r++) {
-                if ((sb.charAt(r) - '0') == k) {
+        for (int q = i; q <= j; q++){
+            int cnt = q;
+            while (cnt != 0){
+                if (cnt % 10 == k){
                     answer++;
                 }
+                cnt /= 10;
+                
             }
         }
-
         return answer;
     }
 }
