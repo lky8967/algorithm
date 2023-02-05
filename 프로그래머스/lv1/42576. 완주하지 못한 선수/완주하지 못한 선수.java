4f1e0daea4737200eb2d1b7c3bf4198c3c95bedf
lky8967 <104722856+lky8967@users.noreply.github.com>
2023-02-05 14:29:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 351.74 ms, Memory: 95.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,15 @@
+import java.util.*;//함수 사용
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for (int i = 0; i < participant.length-1; i++) {
+            if(!participant[i].equals(completion[i]))
+                return participant[i];
+        }
+        return participant[participant.length-1];
+    
+    }
+}
