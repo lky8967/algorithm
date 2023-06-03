aa43982ba350eb9b5546dafd1424478a30b371ae
lky8967 <104722856+lky8967@users.noreply.github.com>
2023-06-04 01:45:21 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.35 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -1,19 +1,14 @@
 
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[5];
-        int cnt = 0;
         Arrays.sort(num_list);
         
-        for(int i = 0 ; i < num_list.length ; i++){
-            if(cnt < 5) {
-                answer[i] = num_list[i];
-                cnt++;
-            }
+        for(int i = 0 ; i < 5 ; i++){
+            answer[i] = num_list[i];
         }
-        
         return answer;
     }
 }
